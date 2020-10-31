24207690d0b0575fb2308af5faedf556ff10d016
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-10-31 13:20:19 +0100

    Fix source set for avatar

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -19,9 +19,10 @@ export default function Avatar() {
   // the side-effect of re-computing `loaded` based on the images' natural width prop
   // We're checking naturalWidth because `onLoad` won't trigger if image is cached
   const isDone = polys >= 100
+  const setLoaded = useCallback(() => isDone && setPolys(102), [isDone])
+
   const img = useRef<HTMLImageElement>()
   const imgEl = img.current
-  const setLoaded = useCallback(() => isDone && setPolys(102), [isDone])
   const loaded = imgEl && imgEl.naturalWidth > 0
 
   // Keeps adding to the number of displayed polygons, until it reached the limit of 100
@@ -32,7 +33,7 @@ export default function Avatar() {
       }
     }
 
-    const raf = requestAnimationFrame(() => setPolys((prev) => prev + 1))
+    const raf = requestAnimationFrame(() => setPolys((prev) => prev + 2))
     return () => cancelAnimationFrame(raf)
   }, [polys, imgEl])
 
@@ -41,13 +42,13 @@ export default function Avatar() {
   return (
     <div className={styles.root}>
       <picture className={styles.picture}>
-        <source srcSet={AVATAR_SRCSET} />
         <SvgAvatar
           className={`${styles.img} ${showImg ? styles.hidden : styles.visible}`}
           numPolys={polys}
         />
         <img
           ref={img}
+          srcSet={AVATAR_SRCSET}
           src={`${AVATAR_BASE_URL}&w=${AVATAR_SIZE}`}
           alt="Espen Hovlandsdal"
           className={`${styles.img} ${showImg ? styles.visible : styles.hidden}`}
