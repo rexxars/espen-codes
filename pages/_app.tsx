01d9431f73db8016bee16c9415db8d55303ce237
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-11-11 15:20:19 +0100

    Add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,16 @@ export default function App({Component, pageProps}: AppProps) {
           name="description"
           content="Espen Hovlandsdal's home on the internet. Blog posts, projects, resume - all that kind of stuff."
         />
+
+        <link rel="icon" href="/favicons/favicon-32.png" sizes="32x32" />
+        <link rel="icon" href="/favicons/favicon-96.png" sizes="96x96" />
+        <link rel="icon" href="/favicons/favicon-128.png" sizes="128x128" />
+        <link
+          rel="icon"
+          href="/favicons/favicon.svg"
+          type="image/svg"
+          sizes="32x32 96x96 128x128"
+        />
       </Head>
 
       <Component {...pageProps} />
